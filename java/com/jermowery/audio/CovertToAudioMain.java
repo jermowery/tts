f273ae76fe5a3fd213a2344e9c228df04f8ab5d9
Jeremy Mowery <jermowery@google.com>
2018-05-20 14:00:57 -0700

    Add class

---
+++ java/com/jermowery/audio/CovertToAudioMain.java
@@ -0,0 +1,8 @@
+package java.com.jermowery;
+
+public class CovertToAudioMain {
+  public static void main(String[] args) {
+    // TODO(jermowery): Have this class actually do something.
+    throw new UnsupportedOperationException("Not implemented");
+  }
+}

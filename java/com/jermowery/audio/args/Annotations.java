47f05eb084a080cbd2ef1c10543426facf383a25
Jeremy Mowery <jermowery@google.com>
2018-05-21 15:32:42 -0700

    Implement arg handling

---
+++ java/com/jermowery/audio/args/Annotations.java
@@ -0,0 +1,16 @@
+package com.jermowery.audio.args;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+public class Annotations {
+  @Target({ElementType.PARAMETER, ElementType.METHOD})
+  @Retention(RetentionPolicy.RUNTIME)
+  public @interface Voice {}
+
+  @Target({ElementType.PARAMETER, ElementType.METHOD})
+  @Retention(RetentionPolicy.RUNTIME)
+  public @interface InputFile {}
+}
---
+++ java/com/jermowery/audio/args/Args.java
@@ -0,0 +1,39 @@
+package com.jermowery.audio.args;
+
+import com.beust.jcommander.IStringConverter;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+@Parameters(separators = "=")
+public class Args {
+
+  @Parameter
+  private List<String> parameters = new ArrayList<>();
+
+  @Parameter(names = "--file", description = "The file to convert", converter = ToFile.class)
+  private File file;
+
+  @Parameter(names = "--voice", description = "The voice model to use")
+  private String voice = "en-GB-Standard-A";
+
+  public File getFile() {
+    return file;
+  }
+
+  public String getVoice() {
+    return voice;
+  }
+
+
+
+  private class ToFile implements IStringConverter<File> {
+
+    @Override
+    public File convert(String value) {
+      return new File(value);
+    }
+  }
+}
